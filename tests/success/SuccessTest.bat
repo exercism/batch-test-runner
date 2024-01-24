5b78fff95081fa4f6f54eff77831be17172e9147
Murat Kirazkaya <77299279+GroophyLifefor@users.noreply.github.com>
2024-01-24 13:40:51 +0300

    feat: refactor tests.

--- tests/success/SuccessTest.bat
+++ tests/success/SuccessTest.bat
@@ -8,31 +8,23 @@ set "slug=Success"
 set "result="
 set "expected=Hello, World!"
 
-REM Run the program and store the output in the result variable
-echo.Looking for "%~dp0%slug%.bat" > log.txt
+REM Check if file exist, if not exit with code 2
 if exist %~dp0%slug%.bat (
-    REM If the file exists in the full path, run it from there
-    echo.Found "%~dp0%slug%.bat" > log.txt
+    REM Run the program and save the output to stdout.bin
     CALL %~dp0%slug%.bat > stdout.bin
+    
+    REM Assign to result varriable the output from stdout.bin
     set /p result=<stdout.bin
+
+    REM Delete stdout.bin
     del stdout.bin
+
 ) else (
+    REM Errorlevel = 2: The system cannot find the file specified. 
+    REM                 Indicates that the file cannot be found in specified location.
+    echo Could not find "%~dp0%slug%.bat" or "%slug%.bat"
+    exit /b 2
 
-    echo.Could not find "%~dp0%slug%.bat" > log.txt
-    echo.Looking for "%slug%.bat" > log.txt
-
-    if exist %slug%.bat (
-        REM If the file exists in the wildcard search path, run it from there
-        echo.Found "%slug%.bat" > log.txt
-        CALL %slug%.bat > stdout.bin
-        set /p result=<stdout.bin
-        del stdout.bin
-    ) else (
-        REM Errorlevel = 2: The system cannot find the file specified. 
-        REM                 Indicates that the file cannot be found in specified location.
-        echo Could not find "%~dp0%slug%.bat" or "%slug%.bat"
-        exit /b 2
-    )
 )
 
 REM Check if the result is correct
